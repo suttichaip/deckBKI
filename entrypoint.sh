4b50fc71cefb25788e1c1d235dee0190560a221d
suttichaip <guy.thunder.guy@gmail.com>
2021-02-03 15:23:05 +0700

    ADD: first  file

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -l
+
+deck ping --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong
+
+deck validate --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong -s kong/default.yaml
+deck sync --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong -s kong/default.yaml
