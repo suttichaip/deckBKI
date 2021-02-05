c6a07bc9c90e3430fa08323c4b2bf06dbc0988d4
Monitor <Monitor@bki.co.th>
2021-02-05 10:36:50 +0700

    EDIT: exit 0

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,5 @@ deck ping --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong
 
 deck validate --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong -s kong/default.yaml
 deck sync --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong -s kong/default.yaml
+
+exit 0
