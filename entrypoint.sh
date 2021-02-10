33a7948ab95d2e536881b3151e406ebacc7c7398
Monitor <Monitor@bki.co.th>
2021-02-10 13:12:26 +0700

    ADD: workspace Ex/In

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,12 @@ deck ping --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong
 deck validate --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong -s kong/default.yaml
 deck sync --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong -s kong/default.yaml
 
+deck validate --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong -s kong/External.yaml
+deck sync --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong -s kong/External.yaml
+
+deck validate --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong -s kong/Internal.yaml
+deck sync --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong -s kong/Internal.yaml
+
 if [ deck ping --kong-addr http://kong-ee:8001 --headers Kong-Admin-Token:kong ]
 then
   exit 0
